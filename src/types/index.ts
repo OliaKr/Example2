9964774127906c2b5fcb3a7f49278f29656c2079
OliaK <olgamr@walla.com>
2024-06-07 00:33:46 +0300

    Added index.ts file for the interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// This file will contain all the interfaces like this that we will import them to our components:
+
+// export interface FilterProps {
+//     manufacturer?: string;
+//     year?: number;
+//     model?: string;
+//     limit?: number;
+//     fuel?: string;
+//   }
+
+// export interface HomeProps {
+//     searchParams: FilterProps;
+//   }
